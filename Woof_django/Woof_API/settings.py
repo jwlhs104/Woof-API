573706bdfe8605acf7f185399908343abd09d8fb
jwlhs104 <johnny85215.cs08g@nctu.edu.tw>
2021-11-21 02:23:58 +0800

    rebase the dir

--- Woof_django/Woof_API/settings.py
+++ Woof_django/Woof_API/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Woof'
+    'corsheaders',
+    'Woof_app'
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000"
 ]
 
 ROOT_URLCONF = 'WoofAPI.urls'
