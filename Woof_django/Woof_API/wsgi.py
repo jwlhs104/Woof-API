e40b4e1305fa51154e7b148f07f1f621e63d219a
jwlhs104 <johnny85215.cs08g@nctu.edu.tw>
2021-11-25 03:02:40 +0800

    change project name in wsgi setting

--- Woof_django/Woof_API/wsgi.py
+++ Woof_django/Woof_API/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for WoofAPI project.
+WSGI config for Woof_API project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WoofAPI.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Woof_API.settings')
 
 application = get_wsgi_application()
