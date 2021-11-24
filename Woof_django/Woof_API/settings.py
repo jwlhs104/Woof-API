04b62475aa81cc9a8df8ed95173a7b99217f2111
jwlhs104 <johnny85215.cs08g@nctu.edu.tw>
2021-11-25 03:21:30 +0800

    change static root

--- Woof_django/Woof_API/settings.py
+++ Woof_django/Woof_API/settings.py
@@ -15,22 +15,16 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'y7v7sne#v_#-67qnb&tr3co3k&8(%-j^!#u3)-fh-k@73-xk3r'
 SECRET_KEY = os.environ.get('SECRET_KEY')
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['fomo-dog.herokuapp.com']
+ALLOWED_HOSTS = ['fomo-dog.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -130,7 +124,8 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'Woof/build/static/')
 
 STATIC_URL = '/static/'
 
